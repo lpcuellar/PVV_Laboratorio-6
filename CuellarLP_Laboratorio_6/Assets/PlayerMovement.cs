6dd1fcdd512024fbddb64c52de53660b95cfc8fe
lpcuellar <lpcuellar14@gmail.com>
2019-02-19 08:27:12 -0600

    Archivos anadids

---
+++ CuellarLP_Laboratorio_6/Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody2D RB2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB2 = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float Xmove = Input.GetAxis("Horizontal");
+        float Ymove = Input.GetAxis("vertical");
+
+        Vector2 movement = new Vector2(Xmove, Ymove);
+
+        RB2.AddForce(movement * speed);
+    }
+}
